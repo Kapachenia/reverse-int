aa8ba5f18955a713cf9518a5106699ed8231f170
Yauheni Kapachenia <kapachenia@gmail.com>
2020-10-25 20:41:42 +0300

    feat: add completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    if(n < 0){
+         n = n * (-1)
+      }
+    const number = String(n);
+     return number.split("").reverse().join('');
+     }
